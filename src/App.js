09a98dcaa93d673731434e3042bfa7436fc429d1
Divij <divij.mehra.16@gmail.com>
2017-06-08 11:17:57 +1000

    charts componenet

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ import logo from './images/logo.svg';
 import './styles/App.css';
 import Calendar from './components/Calendar'
 import Charts from './components/Charts'
+import ChartistGraph from 'react-chartist';
+
+// var lineChartData = {
+//   labels: [1, 2, 3, 4, 5, 6, 7, 8],
+//   series: [
+//     [5, 9, 7, 8, 5, 3, 5, 4]
+//   ]
+// }
+// var lineChartOptions = {
+//   low: 0,
+//   showArea: true
+// }
+
+// var chart = new Chartist.Line('.ct-chart', {
+//   labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+//   series: [
+//     [30, 32, 33, 40, 55, 65, 66, 69, 73, 90],
+//     [25, 29, 33, 41, 53, 58, 62, 65, 71, 88],
+//     [30, 32, 39, 42, 48, 54, 64, 70, 73, 82],
+//     [35, 39, 42, 44, 52, 60, 69, 70, 73, 84]
 
 class App extends Component {
 
@@ -44,8 +64,7 @@ class App extends Component {
          <br/>
          <hr/>
 
-         <Charts
-         />
+        <ChartistGraph data={lineChartData} options={lineChartOptions} type={'Line'} />
 
      </div>
     );
